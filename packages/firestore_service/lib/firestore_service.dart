2de5bda7bc2dbbe19207c033183ed94e1b1cf005
Purva <purvajg@gmail.com>
2021-03-25 23:57:59 -0700

    Fixing null safety issues

--- packages/firestore_service/lib/firestore_service.dart
+++ packages/firestore_service/lib/firestore_service.dart
@@ -8,8 +8,8 @@ class FirestoreService {
   static final instance = FirestoreService._();
 
   Future<void> setData({
-    @required String path,
-    @required Map<String, dynamic> data,
+    required String path,
+    required Map<String, dynamic> data,
     bool merge = false,
   }) async {
     final reference = FirebaseFirestore.instance.doc(path);
@@ -19,44 +19,44 @@ class FirestoreService {
 
   Future<DocumentReference> pushDocument({
 
-    @required String collectionPath,
-    @required Map<String, dynamic> data
+    required String collectionPath,
+    required Map<String, dynamic> data
   } ) async{
     final collectionReference = FirebaseFirestore.instance.collection(collectionPath);
     return await collectionReference.add(data);
   }
 
-  Future<QuerySnapshot> getDocuments<T>({@required String collectionPath}) async {
+  Future<QuerySnapshot> getDocuments<T>({required String collectionPath}) async {
     final reference = FirebaseFirestore.instance.collection(collectionPath);
     print('collectionPath: $collectionPath');
     return reference.get();
   }
 
-  Future<void> deleteData({@required String path}) async {
+  Future<void> deleteData({required String path}) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('delete: $path');
     await reference.delete();
   }
 
-  DocumentReference getDocumentReference({@required String path}) {
+  DocumentReference getDocumentReference({required String path}) {
     return FirebaseFirestore.instance.doc(path);
   }
 
-  CollectionReference getCollectionReference({@required String collectionPath}) {
+  CollectionReference getCollectionReference({required String collectionPath}) {
     return FirebaseFirestore.instance.collection(collectionPath);
   }
 
-  Future<T> getData<T>({@required String path}) async {
+  Future<T?> getData<T>({required String path}) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('get: $path');
     await reference.get();
   }
 
   Stream<List<T>> collectionStream<T>({
-    @required String path,
-    @required T Function(Map<String, dynamic> data, String documentID) builder,
-    Query Function(Query query) queryBuilder,
-    int Function(T lhs, T rhs) sort,
+    required String path,
+    required T Function(Map<String, dynamic> data, String documentID) builder,
+    Query Function(Query query)? queryBuilder,
+    int Function(T lhs, T rhs)? sort,
   }) {
     Query query = FirebaseFirestore.instance.collection(path);
     if (queryBuilder != null) {
@@ -68,7 +68,7 @@ class FirestoreService {
     return snapshots.map((snapshot) {
       print("Found result $snapshot");
       final result = snapshot.docs
-          .where((snapshot) => !snapshot.data().isEmpty)
+          .where((snapshot) => snapshot.data()!.isNotEmpty)
           .map((snapshot) => builder(snapshot.data(), snapshot.id))
           .where((value) => value != null)
           .toList();
@@ -80,8 +80,8 @@ class FirestoreService {
   }
 
   Stream<T> documentStream<T>({
-    @required String path,
-    @required T Function(Map<String, dynamic> data, String documentID) builder,
+    required String path,
+    required T Function(Map<String, dynamic> data, String documentID) builder,
   }) {
     final DocumentReference reference = FirebaseFirestore.instance.doc(path);
     final Stream<DocumentSnapshot> snapshots = reference.snapshots();
